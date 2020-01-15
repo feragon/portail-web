475455ec70d0df6a6cef657cbf548f9bf163fa4f
Florian Roméo <florian.romeo@feragon.net>
2020-01-15 17:15:17 +0100

    Enable firebase module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire';
 
 import { AppComponent } from './app.component';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAsUt6YhF-t2jXX06sMxEc6iSTt9XS9tDM',
+    authDomain: 'portail-web-entreprise.firebaseapp.com',
+    databaseURL: 'https://portail-web-entreprise.firebaseio.com',
+    projectId: 'portail-web-entreprise',
+    storageBucket: 'portail-web-entreprise.appspot.com',
+    messagingSenderId: '1066516793073',
+    appId: '1:1066516793073:web:708c51fe28665c43998b50'
+  },
 };
 
 /*
